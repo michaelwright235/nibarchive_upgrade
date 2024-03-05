f5ee286acc0825c372b4c405d8bec359d3e1f1f3
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-03-06 01:48:50 +0300

    Update main

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,26 @@ use clap::Parser;
 use nibarchive::NIBArchive;
 use nibarchive_upgrade::upgrade;
 
-/// Simple program to greet a person
+/// Convert Apple's NIB Archive .nib to Cocoa Keyed Archive (NSKeyedArchive) .plist.
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
     /// Path to an input NIB Archive .nib file
-    nib_archive_path: String,
+    nib_in: String,
 
     /// Path to an output .plist file
-    plist_path: String,
+    plist_out: String,
 }
 
 pub fn main() -> ExitCode {
     let args = Args::parse();
-    let archive = NIBArchive::from_file(args.nib_archive_path);
+    let archive = NIBArchive::from_file(args.nib_in);
     if let Err(e) = archive {
         println!("{e}");
         return ExitCode::FAILURE;
     }
     let plist_value = upgrade(&archive.unwrap());
-    if let Err(e) = plist_value.to_file_binary(args.plist_path) {
+    if let Err(e) = plist_value.to_file_binary(args.plist_out) {
         println!("{e}");
         return ExitCode::FAILURE;
     }
